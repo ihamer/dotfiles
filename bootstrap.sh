f8f08508532ef426f7b7326886bc40dcb65dc0dc
Ivan <ivanhamer@gmail.com>
2015-02-13 23:11:57 +0000

    Fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
-ln -s .dotfiles/.inputrc ../.inputrc
-ln -s .dotfiles/.bash_profile ../.bash_profile
-ln -s .dotfiles/.bashrc ../.bashrc
-ln -s .dotfiles/.vimrc ../.vimrc
-ln -s .dotfiles/.vim ../.vim
+files=( .inputrc .bash_profile .bashrc .vimrc .vim )
+
+for f in "${files[@]}"; do
+  if [ -h ../$f ]; then
+    rm ../$f
+  else
+    if [ -e ../$f ]; then
+      echo "Moving $f to $f.saved"
+      mv ../$f ../$f.saved
+    fi
+  fi
+  
+  ln -s .dotfiles/$f ../$f
+done
+
