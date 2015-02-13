43415b165d9dccdf161605198660432b2df71b3a
root <ivanhamer@gmail.com>
2015-02-13 23:34:53 +0000

    Cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 files=( .inputrc .bash_profile .bashrc .vimrc .vim )
 
 for f in "${files[@]}"; do
-  if [ -h ../$f ]; then
-    rm ../$f
+  if [ -h $f ]; then
+    rm $f
   else
-    if [ -e ../$f ]; then
+    if [ -e $f ]; then
       echo "Moving $f to $f.saved"
-      mv ../$f ../$f.saved
+      mv $f $f.saved
     fi
   fi
   
-  ln -s .dotfiles/$f ../$f
+  ln -s .dotfiles/$f $f
 done
 
